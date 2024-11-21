d06b5cf083b8a4baff9c356f87dfb0f32aebb48b
basudev96 <basudevshrama604@gmail.com>
2024-11-21 14:55:03 +0530

    commited

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println("Hello World!!===>"+result);
+    }
+}
+
+//create api for send email
+
---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("This is my new Node JS file");
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var port = 3000;
+var server = http.createServer(function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var filename = query['file'];
+    var filepath = path.join(__dirname, filename);
+    fs.readFile(filepath, function (err, data) {
+        if (err) {
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            res.write('<h1>404 Not Found</h1>');
+            res.end();
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        }
+    });
+});
+server.listen(port, function () {
+    console.log('Server is running on port ' + port);
+});
